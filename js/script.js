bf7cf60d5ae484e190971ba76a273a7cc669db5f
vesta228322 <hotabych39@gmail.com>
2026-09-18 10:54:20 +0300

    fix: execute script after DOM is ready

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-const btnDark = document.querySelector('#dark');
-const btnLight = document.querySelector('#light');
-const html = document.documentElement;
+document.addEventListener('DOMContentLoaded', () => {
+    const btnDark = document.querySelector('#dark');
+    const btnLight = document.querySelector('#light');
+    const html = document.documentElement;
 
-const storageTheme = localStorage.getItem('theme');
+    const storageTheme = localStorage.getItem('theme');
 
-if (storageTheme === 'dark') {
-    html.dataset.theme = 'dark';
-    btnDark.classList.add('active');
-    btnLight.classList.remove('active');
-} else {
-    html.dataset.theme = 'light';
-}
+    if (storageTheme === 'dark') {
+        html.dataset.theme = 'dark';
+        btnDark.classList.add('active');
+        btnLight.classList.remove('active');
+    } else {
+        html.dataset.theme = 'light';
+    }
 
-btnDark.addEventListener('click', () => {
-    html.dataset.theme = 'dark';
-    localStorage.setItem('theme', 'dark');
-    btnDark.classList.add('active');
-    btnLight.classList.remove('active');
-});
+    btnDark.addEventListener('click', () => {
+        html.dataset.theme = 'dark';
+        localStorage.setItem('theme', 'dark');
+        btnDark.classList.add('active');
+        btnLight.classList.remove('active');
+    });
 
-btnLight.addEventListener('click', () => {
-    html.dataset.theme = '';
-    localStorage.setItem('theme', 'light');
-    btnLight.classList.add('active');
-    btnDark.classList.remove('active');
+    btnLight.addEventListener('click', () => {
+        html.dataset.theme = '';
+        localStorage.setItem('theme', 'light');
+        btnLight.classList.add('active');
+        btnDark.classList.remove('active');
+    });
 });
