4a0d12cae6538dc916c3f87a82aee442093c2253
vesta228322 <hotabych39@gmail.com>
2026-09-21 17:29:08 +0300

    refactor: update menu cards and responsive styles

--- js/script.js
+++ js/script.js
@@ -27,59 +27,59 @@ document.addEventListener('DOMContentLoaded', () => {
         btnDark.classList.remove('active');
     });
 
-    let cardsData = [];
+    // let cardsData = [];
 
-    async function getData() {
-        try {
-            const res = await fetch('../data.json');
+    // async function getData() {
+    //     try {
+    //         const res = await fetch('../data.json');
 
-            if (!res.ok) {
-                throw new Error(`Ошибка: ${res.status}`);
-            }
-            const data = await res.json();
-            return data
-        } catch (e) {
-            console.error('Что-то пошло не так', e);
-        }
-    }
+    //         if (!res.ok) {
+    //             throw new Error(`Ошибка: ${res.status}`);
+    //         }
+    //         const data = await res.json();
+    //         return data
+    //     } catch (e) {
+    //         console.error('Что-то пошло не так', e);
+    //     }
+    // }
 
-    async function init() {
-        cardsData = await getData();
-        renderCard('coffee');
-    }
+    // async function init() {
+    //     cardsData = await getData();
+    //     renderCard('coffee');
+    // }
 
-    function renderCard(category) {
-        const cardsContent = document.querySelector('.cards__content');
+    // function renderCard(category) {
+    //     const cardsContent = document.querySelector('.cards__content');
 
-        cardsContent.innerHTML = '';
+    //     cardsContent.innerHTML = '';
 
-        const filterCards = cardsData.filter(card => {
-            return card.category === category;
-        });
+    //     const filterCards = cardsData.filter(card => {
+    //         return card.category === category;
+    //     });
 
-        filterCards.forEach(card => {
-            console.log(card);
-            cardsContent.innerHTML += `
-            <div class="cards__item">
-                <div class="cards__photo-wrapper">
-                    <img 
-                        class="cards__photo" 
-                        src="${card.image}" 
-                        alt="${card.alt}"
-                    >
-                </div>
+    //     filterCards.forEach(card => {
+    //         console.log(card);
+    //         cardsContent.innerHTML += `
+    //         <div class="cards__item">
+    //             <div class="cards__photo-wrapper">
+    //                 <img 
+    //                     class="cards__photo" 
+    //                     src="${card.image}" 
+    //                     alt="${card.alt}"
+    //                 >
+    //             </div>
 
-                <div class="cards__text">
-                    <div class="cards__name">${card.name}</div>
-                    <div class="cards__descr">${card.description}</div>
-                    <div class="cards__price">$${card.price.toFixed(2)}</div>
-                </div>
-            </div>
-            `
-        });
-    }
+    //             <div class="cards__text">
+    //                 <div class="cards__name">${card.name}</div>
+    //                 <div class="cards__descr">${card.description}</div>
+    //                 <div class="cards__price">$${card.price.toFixed(2)}</div>
+    //             </div>
+    //         </div>
+    //         `
+    //     });
+    // }
 
-    init();
+    // init();
 
 
 });
